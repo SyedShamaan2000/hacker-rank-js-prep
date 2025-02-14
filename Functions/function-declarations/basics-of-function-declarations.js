a63ffa71f645baa502fd5f42cb64a7bbe871f124
arf <abc@d.c>
2025-02-14 10:28:37 +0530

    basics of functions

---
+++ Functions/function-declarations/basics-of-function-declarations.js
@@ -0,0 +1,33 @@
+// Write a function greet that takes a name as a parameter and returns "Hello, [name]!".
+function greet(name) {
+    return `Hello, ${name}`;
+}
+// console.log(greet("Ahmed"));
+
+// Create a function add that takes two numbers as parameters and returns their sum.
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+// console.log(addNumbers(45, 41));
+
+// Write a function isEven that takes a number and returns true if it is even, otherwise false.
+function checkEven(number) {
+    if(number%2===0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// console.log(checkEven(5));
+
+// Create a function square that takes a number and returns its square.
+function squareNumber(number) {
+    return number*number;
+}
+// console.log(squareNumber(6));
+
+// Write a function sayHello that prints "Hello, World!" without taking any parameters.
+function sayHello() {
+    return "Hello World";
+}
+// console.log(sayHello());
---
+++ Functions/function-declarations/function-parameters-&-return-values.js
@@ -0,0 +1,33 @@
+// Write a function multiply that takes two numbers and returns their product.
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+// console.log(multiplyNumbers(4, 7));
+
+// Create a function maxOfTwo that returns the maximum of two numbers.
+function maxOfTwo(num1, num2) {
+    if(num1>num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+// console.log(maxOfTwo(87, 45));
+
+// Write a function reverseString that takes a string and returns it reversed.
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+// console.log(reverseString("hello"));
+
+// Create a function concatStrings that takes two strings and returns them concatenated.
+function concatStrings(str1, str2) {
+    return str1 + " " + str2;
+}
+// console.log(concatStrings("hello", "world"));
+
+// Write a function calculateRectangleArea that takes width and height as parameters and returns the area.
+function calculateRectangleArea(width, height) {
+    return width * height;
+}
+// console.log(calculateRectangleArea(4, 5));
